28e0e35df7042c1c66de7ea77cf8ec209f067e0e
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-09-22 21:55:02 +0200

    todo, update nav and load data

--- src/MainJS.hx
+++ src/MainJS.hx
@@ -15,20 +15,27 @@ class MainJS {
 	var container:js.html.DivElement;
 
 	public function new() {
-		trace("Hello 'Example Javascript'");
-		init();
-	}
-
-	function init() {
 		document.addEventListener("DOMContentLoaded", function(event) {
 			console.log('${App.NAME} Dom ready :: build: ${App.getBuildDate()} ');
 
 			// var container = document.getElementById("prop");
 			// container.innerHTML = 'html';
 
+			setupNav();
+
 			initHTML();
 			loadData();
 		});
+	}
+
+	function setupNav() {
+		var btnCSS = document.getElementById("btn-css");
+		var btnHTML = document.getElementById("btn-html");
+		var btnJS = document.getElementById("btn-js");
+
+		btnCSS.onclick = () -> loadJson('data/css.json');
+		btnHTML.onclick = () -> loadJson('data/html.json');
+		btnJS.onclick = () -> loadJson('data/js.json');
 	}
 
 	function initHTML() {
@@ -46,6 +53,25 @@ class MainJS {
 		var url = 'data/css.json';
 
 		// var url = 'http://ip.jsontest.com/';
+		var req = new haxe.Http(url);
+		req.onData = function(data:String) {
+			try {
+				var json:Array<FlashCardObj> = haxe.Json.parse(data);
+				trace(json);
+			} catch (e:Dynamic) {
+				trace(e);
+			}
+		}
+		req.onError = function(error:String) {
+			trace('error: $error');
+		}
+		req.onStatus = function(status:Int) {
+			trace('status: $status');
+		}
+		req.request(false); // false=GET, true=POST
+	}
+
+	function loadJson(url:String) {
 		var req = new haxe.Http(url);
 		req.onData = function(data:String) {
 			try {
